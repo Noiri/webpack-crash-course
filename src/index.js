0a0da9111c33658f56254c2d11b3983ada7bf868
Noiri <suleipenn@gmail.com>
2019-03-19 04:59:08 +0900

    implement simple html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function component() {
+  const element = document.createElement('div');
+  const array = ['foo', 'bor']
+  element.innerHTML = _.join(array, '-')
+  return element;
+}
+
+document.body.appendChild(component());
